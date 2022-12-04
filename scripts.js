794411040baa1569855c3f02e4034d43f7104dad
EMTac <115601690+EMTac@users.noreply.github.com>
2022-12-04 14:47:07 -0800

    Added Layer Control

--- scripts.js
+++ scripts.js
@@ -8,14 +8,38 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var marker = L.marker([51.2, 7]).addTo(map);
 
 var sidebar = L.control.sidebar('sidebar', {
-    closeButton: true,
-    position: 'left'
-});
+        closeButton: true,
+        position: 'left'
+    });
+
+sidebar.addTo(map);
+
 map.addControl(sidebar);
 
+setTimeout(function () {
+    sidebar.show();
+}, 500);
+
+// map.on('click', function () {
+//     sidebar.hide();
+// })
+
+// L.geoJSON(POI).addTo(map).on('click', function(e){
+//     sidebar.toggle()
+// });
+
+var POI = L.geoJSON(POI, {
+    onEachFeature: onEachFeature2
+});
+
 var historicLakes = L.geoJSON(Lakes, {
-    onEachFeature: onEachFeature
-}).addTo(map);
+    onEachFeature: onEachFeature,
+    opacity: 0.3,
+});
+
+var lakeComparison = L.geoJSON(Lakes, {
+    onEachFeature: onEachFeature,
+});
 
 function onEachFeature(feature, layer) {
     layer.on('click', function() {
@@ -23,3 +47,28 @@ function onEachFeature(feature, layer) {
     sidebar.setContent(feature.properties.Name);
     });
 }
+
+function onEachFeature2(feature, layer) {
+    layer.on('click', function() {
+    sidebar.show();
+    sidebar.setContent(feature.properties.OBJECTID);
+    });
+}
+
+
+
+var lyrOldTapps = L.imageOverlay('resources/Lake_Tapps_1900.png', [[47.5004398, -121.9953424], [46.9971086, -122.5015877]]);
+
+var tapps1900 = L.layerGroup([tiles, lyrOldTapps, historicLakes]).addTo(map);
+var tapps2022 = L.layerGroup([tiles, lakeComparison, POI]);
+
+var baseMaps = {
+    "1900": tapps1900,
+    "2022": tapps2022,
+};
+
+var overlayMaps = {
+    "1900 Map": tapps1900,
+};
+
+var layerControl = L.control.layers(baseMaps,null,{collapsed:false}).addTo(map);
