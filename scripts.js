fe14cd883f8f250c474fa2c2ed77a1c3b4a83ae3
EMTac <115601690+EMTac@users.noreply.github.com>
2022-12-05 22:57:18 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -28,8 +28,32 @@ setTimeout(function () {
 //     sidebar.toggle()
 // });
 
-var POI = L.geoJSON(POI, {
-    onEachFeature: onEachFeature2
+var myStyleDikes = {
+    "color": "#000000",
+    "weight": 5,
+    "opacity": 1.0,
+    interactive: false,
+};
+
+var myStyleReservoir = {
+    "opacity": 0.3,
+    interactive: false,
+};
+
+var lyrDikes = L.geoJSON(Dikes, {
+    style: myStyleDikes,
+});
+
+var reservoirOverlay = L.geoJSON(tappsReservoir, {
+    style: myStyleReservoir,
+});
+
+var POI1911 = L.geoJSON(POI_1911, {
+    onEachFeature: onEachFeature2,
+});
+
+var POI1900 = L.geoJSON(POI_1900, {
+    onEachFeature: onEachFeature2,
 });
 
 var historicLakes = L.geoJSON(Lakes2, {
@@ -50,22 +74,23 @@ function onEachFeature(feature, layer) {
 }
 
 function onEachFeature2(feature, layer) {
+    var Image=("<img src="+feature.properties.Image+" width=400px"+">");
     layer.on('click', function() {
     sidebar.show();
-    sidebar.setContent("hello");
+    sidebar.setContent("<h1>"+feature.properties.Name+"</h1>"+Image+"<p>"+feature.properties.Caption+"</p>"+"<br></br>"+"<p>"+feature.properties.Description+feature.properties.Description2+"</p>");
     });
 }
 
-
-
-var lyrOldTapps = L.imageOverlay('resources/Lake_Tapps_1900-min.png', [[47.5004398, -121.9953424], [46.9971086, -122.5015877]]);
+var lyrOldTapps = L.imageOverlay('resources/Lake_Tapps_1900.png', [[47.5004398, -121.9953424], [46.9971086, -122.5015877]]);
 
 var tapps = L.layerGroup([tiles]).addTo(map);
-var tapps1900 = L.layerGroup([tiles, lyrOldTapps, historicLakes]);
-var tapps2022 = L.layerGroup([tiles, lakeComparison, POI]);
+var tapps1900 = L.layerGroup([tiles, lyrOldTapps, historicLakes, POI1900]);
+var tapps1911 = L.layerGroup([tiles, lyrOldTapps, POI1911, lyrDikes, reservoirOverlay])
+var tapps2022 = L.layerGroup([tiles, lakeComparison, POI1911]);
 
 var baseMaps = {
     "1900": tapps1900,
+    "1911": tapps1911,
     "2022": tapps2022,
 };
 
@@ -74,3 +99,16 @@ var overlayMaps = {
 };
 
 var layerControl = L.control.layers(baseMaps,null,{collapsed:false}).addTo(map);
+
+map.on('baselayerchange', function(eventLayer) {
+    sidebar.show();
+    if (eventLayer.layer === tapps1900) {
+        map.setView([47.211, -122.175], 14);
+        sidebar.setContent("<h1>"+"Lake Tapps: 1900"+"</h1>"+"<p>"+"What happened to the lake? You will recall that in the present day, Lake Tapps is an expansive reservoir, shaped by human hands. However, traveling back more than a century, we can strip away many of the human modifications to see what came before. What you see now is a collection of natural lakes, remnants of the last ice age. In 1900, these were still heavily forested. The few winding roads in the area are composed of "+"<a href=https://en.wikipedia.org/wiki/Corduroy_road target=_blank>dirt and timber</a>"+", serving the handful of remote farmsteads between Sumner and Buckley."+"</p>"+"<p>"+"The four lakes that underly the modern reservoir are Kirtley Lake, Crawford Lake, Church Lake, and the eponymous Lake Tapps. A 5th, smaller body of water appears here at the northwest corner but remains unnamed. Likely, this was a shallow pond or wetland and is not mentioned in historical accounts of the reservoir’s formation. The other small body of water to the southwest is Lake Bonney, the namesake of the modern city of Bonney Lake, incorporated 50 years later."+"</p>"+"<p>"+"Click on a lake to read more about it or view historical imagery if it is available. Select 1911 when you are ready to move on."+"</p>");
+    } else if (eventLayer.layer === tapps1911) {
+        map.setView([47.211, -122.15], 13);
+        sidebar.setContent("<h1>"+"White River Power Project"+"</h1>"+"<img src= https://digitalcollections.lib.washington.edu/digital/api/singleitem/image/ww-swps/356/default.jpg?highlightTerms=lake%20tapps width=400px>");
+    } else if (eventLayer.layer === tapps2022) {
+        sidebar.setContent("<h1>"+"The Passing of a Century"+"</h1>")
+    }
+  });
